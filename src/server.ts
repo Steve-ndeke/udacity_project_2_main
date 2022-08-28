831b99834171340c4e63857d7bac3c323dda6188
Steve-ndeke <steve.ndeke1@accesscfa.com>
2022-08-28 13:33:55 +0300

    modified filter image function2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {Request,Response } from 'express';
+import {Router,Request,Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -29,8 +29,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get('/filtereimage', async (req: Request, res: Response) =>{
-    const image_url = req.query.image_url.tostring();
+  app.get('/filteredimage', async (req: Request, res: Response) =>{
+    const image_url = req.query.image_url.toString();
     if (!image_url){
       res.status(400).send('Image URL is Required');
     }
@@ -39,7 +39,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
     res.status(200).sendFile(filtered_image, () => {
       deleteLocalFiles([filtered_image]); 
-    })
+    });
       
     
   });
